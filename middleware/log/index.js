5412bc509fb0d52b63b72c42f0cd8045b2be9c42
hxhgta <hxhgta@163.com>
2016-12-24 19:59:48 +0800

    update

--- middleware/log/index.js
+++ middleware/log/index.js
@@ -1,4 +1,4 @@
 module.exports = function (req, res, next) {
-  // console.log(new Date().toString() + ': ' + req.originalUrl);
+  console.log(new Date().toString() + ': ' + req.originalUrl);
   next();
 };
